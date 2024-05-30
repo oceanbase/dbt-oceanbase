ae4f08d2f8c25a183f1ae5368114272dcc5538e1
yh263208 <yh263208@oceanbase.com>
2024-05-30 17:24:00 +0800

    correct the error config

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-pytest_plugins = ["dbt.tests.fixtures.project", "tests.functional.fixtures"]
+pytest_plugins = ["dbt.tests.fixtures.project"]
