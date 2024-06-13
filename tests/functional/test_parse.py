5742321294e89bbe0c94aab137ba6df3f0df3830
yh263208 <yh263208@oceanbase.com>
2024-06-13 15:22:47 +0800

    add test cases

--- tests/functional/test_parse.py
+++ tests/functional/test_parse.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import pytest
+
 from dbt.cli.main import dbtRunner, dbtRunnerResult
 from tests.functional.utils import BaseOBMySQLTestCase
 
@@ -20,3 +22,23 @@ class TestParse(BaseOBMySQLTestCase):
     def test_no_content_parse_succeed(self, project, dbt: dbtRunner):
         res: dbtRunnerResult = dbt.invoke(args=["parse"])
         assert res.success == True
+
+
+class TestParseWithModels(BaseOBMySQLTestCase):
+
+    __MODEL_SQL = """
+    select 1 from dual
+    """
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "model_one.sql": self.__MODEL_SQL,
+            "model_two.sql": self.__MODEL_SQL,
+            "model_three.sql": self.__MODEL_SQL,
+        }
+
+    def test_three_models_parse_succeed(self, project, dbt: dbtRunner, models):
+        res: dbtRunnerResult = dbt.invoke(args=["parse"])
+        assert res.success == True
+        assert len(res.result.nodes) == len(models)
